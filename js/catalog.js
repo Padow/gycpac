158a6cc98bd31ea3e25ff723b3bb9d0c2e069554
padow <patrick.douchement@gmail.com>
2016-09-30 11:33:01 +0200

    clean catalog ctrl

--- js/catalog.js
+++ js/catalog.js
@@ -4,25 +4,4 @@
 */
 app.controller("Catalog",function($scope, socket) {
 // stuff
-  $scope.listDatabases = function(){
-    socket.emit("listDatabases", {}, function (err, res){
-       console.log(res);
-       console.log(err);
-    });
-  }
-
-  var datacsv = false;
-    $scope.file_changed = function(element) {
-        datacsv = true;
-        var csv = element.files[0];
-        var reader = new FileReader();
-        reader.onload = function(e) {
-          $scope.$apply(function() {
-            $scope.prev_orchestrator = e.target.result;
-            var data = e.target.result;
-            socket.emit('uploadcsv', {data: data});
-          });
-        };
-        reader.readAsDataURL(csv);
-     };
 });
